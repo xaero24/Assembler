bc540e72951a9ca27dc6412870155a00bf9705ae
Mike A <michaelafu@gmail.com>
2020-02-01 20:42:41 +0200

    initial commit
    Some header details and initial code

---
+++ main.c
@@ -0,0 +1,9 @@
+
+#include "parser.c"
+
+int main(int arcg, char* argv[]){
+    char filename[256];
+    int error = FALSE;
+
+
+}
---
+++ main.h
@@ -0,0 +1,37 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+//Boolean operators
+#define TRUE 1
+#define FALSE 0
+
+//Opcodes group 1 - dual operands
+#define MOV 0
+#define CMP 1
+#define ADD 2
+#define SUB 3
+#define LEA 4
+
+//Opcodes group 2 - single operand
+#define CLR 5
+#define NOT 6
+#define INC 7
+#define DEC 8
+#define JMP 9
+#define BNE 10
+#define RED 11
+#define PRN 12
+#define JSR 13
+
+//Opcodes group 3 - no operands
+#define RTS 14
+#define STOP 15
+
+//Various definitions for parsing
+#define LINE_LEN 80
+#define BLANK_LINE 0
+#define COMMENT 1
+#define DIRECTION 2
+#define OPERATION 3
+
+#endif
